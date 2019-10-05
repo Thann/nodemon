edb946f5064c9a4205cee32d46ba93ddb33d4f44
Jonathan Knapp <thann@purse.io>
2019-10-05 09:57:26 -0700

    add cli

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ const path = require('path');
 const chokidar = require('chokidar');
 const { fork } = require('child_process');
 
-module.exports = function(arg) {
+module.exports = function(opts) {
   if (!process.send) {
-    let child;
+    let child, mod;
+    if (require.main === module) {
+      opts = path.dirname(process.argv[2]);
+      mod = path.join(process.cwd(), process.argv[2]);
+    } else if (!opts) {
+      opts = path.dirname(process.argv[1]);
+    }
     const start = function() {
       if (child)
         child.kill();
-      child = fork(process.argv[1]);
+      child = fork(mod || process.argv[1]);
     };
-    if (!arg) {
-      arg = path.dirname(process.argv[1]);
-    }
-    chokidar.watch(arg).on('ready', start).on('change', start);
+    chokidar.watch(opts).on('ready', start).on('change', start);
   }
   return !process.send;
+}
+
+if (require.main === module) {
+  module.exports();
 }
