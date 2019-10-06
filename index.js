8b511920e8d997c36e97968398516deef8fd54a3
Jonathan Knapp <jaknapp8@gmail.com>
2019-10-05 17:00:22 -0700

    rename param

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const path = require('path');
 const chokidar = require('chokidar');
 const { fork } = require('child_process');
 
-module.exports = function(bin, opts) {
+module.exports = function(paths, opts) {
   if (!process.send) {
     let child, mod;
     if (require.main === module) {
-      bin = path.dirname(process.argv[2]);
+      paths = path.dirname(process.argv[2]);
       mod = path.join(process.cwd(), process.argv[2]);
-    } else if (!bin) {
-      bin = path.dirname(process.argv[1]);
+    } else if (!paths) {
+      paths = path.dirname(process.argv[1]);
     }
     const start = function() {
       if (child)
         child.kill();
       child = fork(mod || process.argv[1]);
     };
-    chokidar.watch(bin, opts).on('ready', start).on('change', start);
+    chokidar.watch(paths, opts).on('ready', start).on('change', start);
   }
   return !process.send;
 }
